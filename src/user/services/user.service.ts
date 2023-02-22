ffe564688a0a6c6b33cec0431c2058d7da005721
sunil kumar mutaka <carbojet@gmail.com>
2023-02-22 12:48:18 +0530

    GET end point fixed

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../user.model';
 import { Model } from 'mongoose';
-import * as mongoose from 'mongoose';
-
 @Injectable()
 export class UsersService {
   private Users: User[] = [];
@@ -16,37 +14,41 @@ export class UsersService {
       if (result) {
         return res.status(201).json({
           message: 'User Created Successfully',
+          data: true,
           result: result,
         });
       } else {
         return res.status(409).json({
-          message: 'User Not Created. Something Problem',
+          message: 'User Not Created. Something went wrong',
+          data: false,
         });
       }
-    } catch (error){
-      return res.status(409).json({
+    } catch (error) {
+      return res.status(500).json({
         message: error.message,
+        data: false,
       });
     }
   }
   async getUser(id: string, res: any) {
     try {
-      const objectId = mongoose.Types.ObjectId;
-      const result = await this.userModel.findById({ id: objectId }).exec();
-      console.log(result);
+      const result = await this.userModel.findById({ _id: id });
       if (result) {
         return res.status(201).json({
           message: 'User Created Successfully',
+          data: true,
           result: result,
         });
       } else {
         return res.status(409).json({
-          message: 'User Not Created. Something Problem',
+          message: 'User Not Created. went wrong',
+          data: false,
         });
       }
     } catch (error) {
-      return res.status(409).json({
+      return res.status(500).json({
         message: error.message,
+        data: false,
       });
     }
   }
